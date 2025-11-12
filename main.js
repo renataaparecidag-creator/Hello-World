cf45ce064c3a7fdd7b0c2a4658e08a8fdb6c235a
renataaparecidag-creator <renataaparecidag@prof.educacao.sp.gov.br>
2025-11-12 10:29:04 -0300

    Construção do main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-let nomeUsuário = prompt("Qual o seu nome?");
+let nomeUsuário = "";
 let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = "seja muito bem-vindo!";
+}else{
 elemento.textContent = nomeUsuário;
+}
+
